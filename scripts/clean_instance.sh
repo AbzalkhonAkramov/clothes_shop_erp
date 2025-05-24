2aa1d99cd386a6fc54c3f79f20549830cfe19745
Abzalhon Akramov <akramovabzalxon@gmail.com>
2025-05-24 15:13:19 +0500

    ci/cd 2

--- scripts/clean_instance.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rm -rf /home/ubuntu/clothes_shop_erp/*
--- scripts/gunicorn.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-sudo cp /home/ubuntu/clothes_shop_erp/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/clothes_shop_erp/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
-
-sudo systemctl start gunicorn.service
-sudo systemctl enable gunicorn.service
